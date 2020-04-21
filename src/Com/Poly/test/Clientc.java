8f82531525c08f17dfa2c604217a49e48ed9f633
Ankita S. Kotkar <akotkar@dc2.propsol.org>
2020-04-21 10:17:10 +0530

    Polymorphism Project

---
+++ src/Com/Poly/test/Clientc.java
@@ -0,0 +1,10 @@
+package Com.Poly.test;
+
+public class Clientc {
+
+	public static void main(String[] args) {
+		firstc obj=new firstc();
+		obj.m1(5);
+		obj.m1(0.5);
+	}
+}
---
+++ src/Com/Poly/test/firstc.java
@@ -0,0 +1,17 @@
+package Com.Poly.test;
+
+public class firstc {
+
+	int a=10;
+	double b;
+	
+void m1(int aa) {
+	a =aa;
+	System.out.println("a :"+a);
+}
+
+void m1(double bb) {
+	b=bb;
+	System.out.println("b :"+b);
+}
+}
